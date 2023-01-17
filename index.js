5bb5381fb3d8dc0bf5d7d03112f4f737a468e544
Jessica Andersen <jessicajoyandersen@gmail.com>
2023-01-17 16:38:46 -0500

    changed console messages

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let selectedColor = 'blue';
 const painting = document.querySelector('.painting');
 painting.addEventListener('click', function(e){
     e.target.style.backgroundColor = selectedColor;
-    console.log('e.target_color');
+    console.log('paint');
 })
 
 const blue = document.querySelector('#blue');
@@ -11,18 +11,18 @@ const yellow = document.querySelector('#yellow');
 const green = document.querySelector('#green');
 
 blue.addEventListener('click', function(e){
-    console.log('e.target_change');
+    console.log('color_blue');
     selectedColor = 'blue'
 })
 red.addEventListener('click', function(e){
-    console.log('e.target_change');
+    console.log('color_red');
     selectedColor = 'red'
 })
 yellow.addEventListener('click', function(e){
-    console.log('e.target_change');
+    console.log('color_yellow');
     selectedColor = 'yellow'
 })
 green.addEventListener('click', function(e){
-    console.log('e.target_change');
+    console.log('color_green');
     selectedColor = 'green'
 })
