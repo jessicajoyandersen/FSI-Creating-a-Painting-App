2bc0200c834df74a89ea5b84f3c3f4266d3da660
Jessica Andersen <jessicajoyandersen@gmail.com>
2023-01-17 16:36:51 -0500

    finally, full functionality!

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-let selectedColor = 'blue'
-const painting = document.querySelector('.painting')
-
-const palette = document.querySelector('.palette')
-
+let selectedColor = 'blue';
+const painting = document.querySelector('.painting');
 painting.addEventListener('click', function(e){
-    e.target.style.backgroundColor = 'blue'
+    e.target.style.backgroundColor = selectedColor;
+    console.log('e.target_color');
 })
 
+const blue = document.querySelector('#blue');
+const red = document.querySelector('#red');
+const yellow = document.querySelector('#yellow');
+const green = document.querySelector('#green');
+
+blue.addEventListener('click', function(e){
+    console.log('e.target_change');
+    selectedColor = 'blue'
+})
+red.addEventListener('click', function(e){
+    console.log('e.target_change');
+    selectedColor = 'red'
+})
+yellow.addEventListener('click', function(e){
+    console.log('e.target_change');
+    selectedColor = 'yellow'
+})
+green.addEventListener('click', function(e){
+    console.log('e.target_change');
+    selectedColor = 'green'
+})
